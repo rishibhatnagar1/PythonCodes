6e2618f98bf768322b6dd7b78cb970fad0c664df
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-04-17 10:33:44 +0530

    short bubble sort added

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,14 +3,30 @@
 alist1 =[10,20,30,40,1,2,3,34634,0,10,10,10,10,10,5,3,2,4,5,6,7,8,9,0,5,3,2,3,123,12,3,123,123,12,3,123,1,23,123,123,121,122]
 
 def bubbleSort(alist):
-	for passnum in range(len(alist)-1,0,-1): #here the val of passnum will be the total length, another loop to run from 0 to max val of length
+	for passnum in range(len(alist)-1,0,-1): #here the val of passnum will be the total length, it will decrement from the end basically
 		for i in range(0,passnum):
 			if alist[i]>alist[i+1]:
 				temp = alist[i]
 				alist[i] =alist[i+1]
 				alist[i+1] = temp
-				#print alist[i],alist[i+1] 
+				
 	print alist			
+def shortBubbleSort(alist):
+	exchanges = True
+	passnum = len(alist)-1
+        while passnum > 0 and exchanges:
+		exchanges = False
+       		for i in range(passnum):
+           		if alist[i]>alist[i+1]:
+               			exchanges = True
+               			temp = alist[i]
+               			alist[i] = alist[i+1]
+               			alist[i+1] = temp
+       		passnum = passnum-1
+	print alist
+
+shortBubbleSort(alist1)
 bubbleSort(alist1)
-#print(alist1)			
+
+	
 	
