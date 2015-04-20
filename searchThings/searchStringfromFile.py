fba9eebbf93b6a734fb774fe895b014e8a799861
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-04-20 16:53:54 +0530

    n

--- searchThings/searchStringfromFile.py
+++ searchThings/searchStringfromFile.py
@@ -4,8 +4,7 @@ import re
 lines = open( sys.argv[1], "r" ).readlines()
 count = 0
 for line in lines:
-	
-	if re.search( r"sex", line ):
+	if re.search( r"heaven", line ):
 		print line
 		count +=1
 print "Total count",count
