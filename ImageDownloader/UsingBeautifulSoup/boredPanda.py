9a4da955526b94fdc252041949f097b5baffa201
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-04-29 00:02:42 +0530

    ImageDownloader working, added readme and working code

--- ImageDownloader/UsingBeautifulSoup/boredPanda.py
+++ ImageDownloader/UsingBeautifulSoup/boredPanda.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-''' Going to attempt to download images from Steve McCurry, that come in as google image search '''
-''' Google won't let you scrape the data. FYI '''
+''' Going to attempt to download images from Bored Panda '''
 link =raw_input("Please Drop link of the gallery here: ")
 name = raw_input("Name it: ")
 ''' Make a request to the link to get all the source code '''
@@ -13,24 +12,25 @@ soup = BeautifulSoup(r.content)
 ''' We could print this using the following command '''
 #print soup.prettify()
 ''' Find the position of the image, the following gives the script which has the image in it '''
-g_data = soup.find_all("a")
+g_data = soup.find_all("img")
 #print g_data
 ''' Save Image '''
 count = 0 #This will help in saving the files with an index
 def saveImage(dlink):
         res =requests.get(dlink)
         if res.status_code == 200:
-                    with open(name+str(count)+".jpg", 'wb') as f:
-                        for chunk in res.iter_content():
-                            f.write(chunk)
+		with open(name+str(count)+".jpg", 'wb') as f:
+			for chunk in res.iter_content():
+				f.write(chunk)
 
 ''' The img tags will have attributes which need to be accessed, they can be using .attrs[""] we do not need to go and look for the application/json at the bottom of the page '''
 for aTag in g_data:
-	if "href" in aTag.attrs and "photos" in aTag.attrs["href"]:
-		#print aTag.attrs["href"]
-		count +=1
-		dlink = aTag.attrs["href"]
-		saveImage(dlink)
-		print "."
+	if "src" in aTag.attrs  :
+		if ".jpg" in aTag.attrs["src"]:
+			#print aTag.attrs["src"]
+			count +=1
+			dlink = aTag.attrs["src"]
+			saveImage(dlink)
+			print "."
 count = 0
 print "All images saved"
--- ImageDownloader/UsingBeautifulSoup/readme.md
+++ ImageDownloader/UsingBeautifulSoup/readme.md
@@ -1,3 +1,15 @@
-#500px scraper working 
+#Scraping Images 
 
-I am attempting to learn more about web scraping with python and Beautiful soup looks like the tool I was looking for. Being said that, I am struggling with cleaning up the data, here I will attempt to keep the code clean and comment everything. 
+The files in the folder are all working for now. However more testing is required, as it is said, you code will all work, until, one day, it doesn't and then you are screwed. So anyways here are the latest upates: 500px, bored panda and stevemccury are all working.
+``` ```
+``` ```
+500px : Downloads the image from a link provided, this will be from the explore section. Click on the image to enlarge it and then put the link of the same in. You can use any name for this.
+``` ```
+``` ```
+boredPanda: The script downloads all the .jpg files on the page. Works mostly. Needs to be tested again.
+``` ```
+``` ```
+Steve Mc curry: I love and respect his work, wanted some inspiration, hence the script. oYou will have to go to the grid view and then put the link in.
+``` ```
+``` ```
+
