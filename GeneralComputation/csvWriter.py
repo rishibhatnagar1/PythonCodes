5b9a52a64b6db1879088de97c6930d3109f43316
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-06-20 18:34:42 +0530

    added how to vim

--- GeneralComputation/csvWriter.py
+++ GeneralComputation/csvWriter.py
@@ -13,16 +13,19 @@ def csv_dict_writer(path, fieldnames, data):
  
 #----------------------------------------------------------------------
 if __name__ == "__main__":
-    data = ["first_name,last_name,city".split(","),
-            "Tyrese,Hirthe,Strackeport".split(","),
+    data0 = ["prodId,prodName,avgRating,review,reviewRating".split(",")]
+    data1 =[
+            "Tyrese,Hirthe,Strackeport,blah,blahha,blhahaha".split(","),
             "Jules,Dicki,Lake Nickolasville".split(","),
             "Dedric,Medhurst,Stiedemannberg".split(",")
             ]
     my_list = []
-    fieldnames = data[0]
-    for values in data[1:]:
+   # Give the Field Names, the column names if I may
+    fieldnames = data0[0] 
+   # The values inside the columns, they go row wise, as can be seen above. 
+    for values in data1[0:]:
         inner_dict = dict(zip(fieldnames, values))
         my_list.append(inner_dict)
- 
-    path = "dict_output.csv"
+   # print my_list
+    path = "mod_output.csv"
     csv_dict_writer(path, fieldnames, my_list)
